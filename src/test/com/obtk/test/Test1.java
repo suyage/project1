9561c36737095309160c9819628fd819493d93d3
suyage <qaz2868211047@tom.com>
2020-12-28 14:10:26 +0800

    Initial commit

---
+++ src/test/com/obtk/test/Test1.java
@@ -0,0 +1,21 @@
+package com.obtk.test;
+
+import org.junit.Test;
+
+/**
+ * @author Xiaobei
+ * @date 2020-12-24 09:10:40
+ */
+public class Test1 {
+    @Test
+    public void textjk(){
+        System.out.println("jjjjjj");
+    }
+
+    @Test
+    public void text1(){
+        System.out.println("你苏爸爸来咯！");
+    }
+
+
+}
